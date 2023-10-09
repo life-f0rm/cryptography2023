46b61fcf82429bc9c4d4caa1ce697f86fbd06158
k1 <44195422+Life-F0rM@users.noreply.github.com>
2023-10-09 13:06:17 +0300

    Add comments

--- hw1_mod_mult_inverse.py
+++ hw1_mod_mult_inverse.py
@@ -14,13 +14,13 @@ def extended_gcd(a, b):
         and the coefficients x and y such that ax + by = gcd(a, b).
     """
     # Initialize the coefficients
-    x0, x1, y0, y1 = 1, 0, 0, 1
+    x0, x1, y0, y1 = 1, 0, 0, 1 # Initialize x0, x1, y0, y1
 
     # Perform the Extended Euclidean Algorithm
-    while b != 0:
-        q, a, b = a // b, b, a % b
-        x0, x1 = x1, x0 - q * x1
-        y0, y1 = y1, y0 - q * y1
+    while b != 0: # Loop until b becomes zero. If b = 0, then a = gcd
+        q, a, b = a // b, b, a % b # quotient = a // b, a = b and b = remainder of a / b
+        x0, x1 = x1, x0 - q * x1 # save x1 as x0, save x1 as x0 - quotient * x1
+        y0, y1 = y1, y0 - q * y1 # Do same for y.
 
     # Return the result
     return a, x0, y0
@@ -42,10 +42,10 @@ def mod_inverse(a: int, m: int) -> int:
     if gcd != 1:
         return None  # Modular multiplicative inverse does not exist
     else:
-        return x % m
-
+        return x % m # Modular multiplicative inverse = reminder of coefficient x / modulo
+    
 # Test the function
 a = 19
 m = 97
 inverse = mod_inverse(a, m)
-print("The modular multiplicative inverse of {} modulo {} is {}".format(a, m, inverse))
+print(f"The modular multiplicative inverse of {a} modulo {m} is {inverse}")
